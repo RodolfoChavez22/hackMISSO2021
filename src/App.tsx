0ebf770efbb7ccc40e80aa457a90155ae56e12a9
LeftAutomated <bryantle2021@gmail.com>
2021-11-11 13:03:15 -0600

    setup client src

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Container, Typography } from '@material-ui/core';
+import styles from './styles';
 
 function App() {
+  const classes = styles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center"> Data Visualize </Typography>
+      </AppBar>
+    </ Container>
   );
 }
 
